f3ef7de99be760b2e36b83d64c813f29dfa27196
JUEST4 <juest4@pitt.edu>
2025-01-26 17:31:13 -0500

    added cmd line args

--- Run_ILI_Tracker.py
+++ Run_ILI_Tracker.py
@@ -6,17 +6,35 @@ from Misc import *
 from datetime import date
 from ILI_Tracker import ili_tracker
 import matplotlib.pyplot as plt
+import argparse
 import numpy as np
 
-# ------------------------------------------------------------------------
 
+# Default
 data_directory = './data'
 data_file = 'Sample_Data.csv'
 diseases = ['INFLUENZA','RSV','HMPV','PARAINFLUENZA','OTHER']
+
 ll_fields = [disease+'_loglikelihood_T' for disease in diseases]
 priors = normalize([(0.1/(len(diseases)-1)) if dx!='OTHER' else 0.9 for dx in diseases],1.0)
 admission_date_field, delimiter, file_missing_value, data_missing_value, base = 'Admit_date_time', ',', 'M', 'M', 10.0
 equivalent_sample_size, moving_average_window = 10, 7
+
+# ------------------------------------------------------------------------
+# Command Line Arguments
+# ------------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='ILI Tracker')
+parser.add_argument('--data_directory', type=str, default='./data', help='Directory containing the data file')
+parser.add_argument('--data_file', type=str, default='Sample_Data.csv', help='Data file')
+parser.add_argument('--diseases', type=str, default='INFLUENZA,RSV,HMPV,PARAINFLUENZA,OTHER', help='List of diseases')
+
+args = parser.parse_args()
+if args.diseases:
+    diseases = args.diseases.split(',')
+if args.data_directory:
+    data_directory = args.data_directory
+if args.data_file:
+    data_file = args.data_file
 
 # ------------------------------------------------------------------------
 
